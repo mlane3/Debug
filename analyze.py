7bfa8b00583fe943e8e5821f3294d72bb8f0b3db
mlane3 <mlane3@gatech.edu>
2012-09-13 05:50:55 -0400

    sdf

--- analyze.py
+++ analyze.py
@@ -16,52 +16,57 @@ filename = 'Data1.txt'
 #datafile.readline()
 x = []
 y = []
-
-print 'test'
-
 csvfile = csv.reader( open(filename,'r'), delimiter='\t')
 csvfile.next()
 for row in csvfile:
     x.append( float(row[0]) )
     y.append( float(row[1]) )
-#for row in datafile.readlines():
-        #row = row.strio().split('\t') # does this go here? My friend suggested it
-	#x.append(float(row[1]))
-	#y.append(-float(row[0]))
-print row
-print len(x)
+print('length of x',len(x))
 
 
 # Smooth The data
-binsize = 64
+binsize = 8
 x2 = []
 y2 = []
 for i in range(binsize//2,len(x)-binsize//2):
-	xtemp = x[i-binsize//2:i+binsize//2]
-	ytemp = y[i-binsize//2:i+binsize//2]
-	x2.append(sum(xtemp)/len(xtemp))
-	y2.append(sum(ytemp)/len(ytemp))
+    xtemp = x[i-binsize//2:i+binsize//2]
+    ytemp = y[i-binsize//2:i+binsize//2]
+    x2.append(sum(xtemp)/len(xtemp))
+    y2.append(sum(ytemp)/len(ytemp))
 print len(x2)
 
 #Find Maximums
-maxx = []
+maxx = [] #extrimums
 maxy = []
-for n in range(1,len(x2)-1):
-	if y2[n-1]<y2[n] and y2[n+1]<y2[n]:
-		maxx.append(x2[n])
-		maxy.append(y2[n])
-print len(maxx)
-#plt.figure(1, figsize=(5,4))
-diffx = []
-diffy = []
-for n in range(2,len(maxx)):
-    if math.fabs(maxx[n]-maxx[n-1]) > .1:
-        ytemp = math.fabs(maxy[n] -maxy[n-1])
-        xtemp = math.fabs(maxx[n] -maxx[n-1])
-        diffx.append(xtemp)
-        diffy.append(ytemp)
-plt.scatter(maxx,maxy)
-#plt.plot(x,y)
-#plt.axis( (theta_exp[0],theta_exp[-1],0,1.1) )
+minx = []
+miny = []
+for n in range(1,len(x2)-32):
+    if y2[n-1]<y2[n] and y2[n+1]<y2[n] and y2[n-32]<y2[n] and y2[n+32]<y2[n]:
+        maxx.append(x[n])
+        maxy.append(y[n])
+    if y2[n-1]>y2[n] and y2[n+1]>y2[n] and y2[n-32]>y2[n] and y2[n+32]>y2[n]:
+        minx.append(x[n])
+        miny.append(y[n])
+
+print('maxx',len(maxx))
+print('minx',len(minx))
+timeofevent = x[-1] - x[1]
+Peak = sum(maxy)/len(maxy) - sum(miny)/len(miny)
+print('timeofevent =',timeofevent,'s')
+print('Pk-Pk (V) =',Peak)
+for i2 in range(1, len(maxx)):
+    print repr(maxx[i2]).rjust(2), repr(maxy[i2]).rjust(3)
+print '---------'
+for n in range(1, len(minx)):
+    print repr(minx[n]).rjust(2), repr(miny[n]).rjust(3)
+plt.figure(1, figsize=(5,4))              
+plt.xlabel('Measured Time (s)')
+plt.ylabel('Detected Interference Intensity (V)')
+plt.title('Interometry plot (fringes as arm pathlength changes)')
+plt.scatter(maxx,maxy,color = 'r')
+plt.scatter(minx,miny,color = 'g')
+#plt.plot(x2,y2,ls='-',color = 'g')
+#plt.plot(x,y,marker='o',ls='-')
+plt.plot(x2,y2,ls='-')
+#plt.scatter(x,y,marker='o', color ='b')
 plt.show()
-print 'test'
--- analyze1.py
+++ analyze1.py
@@ -10,7 +10,7 @@ rc('text', usetex=True)
 print 'test'
 
 # Open the data
-filename = 'gas 1-2.txt'
+filename = 'gas 10.txt'
 #datafile = open(filename, 'r')
 #datafile.readline()
 x = []
@@ -24,16 +24,12 @@ for row in csvfile:
     xtemp = float(row[0]) + float(row[1])/60
     x.append( xtemp )
     y.append( float(row[2]) )
-#for row in datafile.readlines():
-        #row = row.strio().split('\t') # does this go here? My friend suggested it
-	#x.append(float(row[1]))
-	#y.append(-float(row[0]))
 print row
 print len(x)
 
 
 # Smooth The data
-binsize = 48
+binsize = 64
 x2 = []
 y2 = []
 for i in range(binsize//2,len(x)-binsize//2):
@@ -51,9 +47,15 @@ for n in range(1,len(x2)-1):
 		maxx.append(x2[n])
 		maxy.append(y2[n])
 
-#plt.figure(1, figsize=(5,4))
-plt.plot(x2,y2)
-plt.scatter(maxx,maxy)
+plt.figure(1, figsize=(5,4))
+#plt.scatter(x,y, linestyle='-'
+plt.plot(x,y,marker='o',ls='-')
+plt.plot(x2,y2,ls='-',c='red')
+plt.xlabel('Measured Time (s)')
+plt.ylabel('Detected Interference Intensity (V)')
+plt.title('Interometry plot (fringes as arm pathlength changes)')
+
+
 
 #plt.axis( (theta_exp[0],theta_exp[-1],0,1.1) )
 plt.show()
